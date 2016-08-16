bdf446afcc146d924d0d0ad6362abfdd45078174
Anthony Roy <anthony@techscape.net>
2016-08-16 15:14:31 +0100

    More ideas

--- smashit.py
+++ smashit.py
@@ -3,23 +3,24 @@
 import random
 
 class SmashItEngine:
-    def __init__(self, ui):
+    def __init__(self, ui, choice_function=random.choice):
         self.ui = ui
+        self.choice_function = choice_function
         self.actions = ["bopit", "smashit", "twizzleit"]
         self.sequence = []
 
     def start(self):
-        self.ui.say("start")
+        self.ui.get_response_to("start")
 
     def add_action(self):
-        self.sequence.append(random.choice(self.actions))
+        self.sequence.append(self.choice_function(self.actions))
 
     def take_turn(self, response_time):
         """This method should sequence the things required during the turn. e.g.
         1. Add an action to the sequence
         2. For each action in the sequence:
             a. tell the UI to let the user know which action to do.
-            b. The UI should return a success or fail response from the "say" method.
+            b. The UI should return a success or fail response from the "get_response_to" method.
             c. There should be a timeout on the response, after which we assume the user failed
             and we abort the loop with a fail. THIS IS THE HARD BIT!
         3. If we failed, tell the ui that we failed, and end the game.
@@ -35,11 +36,24 @@ class SmashItTextUI:
             "smashit": "Smash It! (press S)",
             "twizzleit": "Twizzle It! (press T)"
         }
+        self.action_expected = {
+            "bopit": "B",
+            "smashit": "S",
+            "twizzleit": "T"
+        }
 
-    def say(self):
-        pass
+    def get_response_to(self, action):
+        print(self.responses[action])
+
+        user_response = self.get_response_from_user()
+        expected_response = self.action_expected[action]
+
+        return user_response == expected_response
+
+    def get_response_from_user(self):
+        return "B"
 
     def fail(self):
-        pass
+        print("You Lose!")
 
 
