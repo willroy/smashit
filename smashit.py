d578a2e72e69e19b716480e70d1f03df47cf246c
William Roy <willjoroy@gmail.com>
2016-08-19 09:20:58 +0100

    Got rid of unneeded comment.

--- smashit.py
+++ smashit.py
@@ -33,15 +33,7 @@ class SmashItEngine:
         action = self.next_action()
         return self.ui.get_response_to(action, response_time)
         
-        """This method should sequence the things required during the turn. e.g.
-        1. Add an action to the sequence DONE
-        2. For each action in the sequence: 
-            a. tell the UI to let the user know which action to do. 
-            b. The UI should return a success or fail response from the "get_response_to" method.
-            c. There should be a timeout on the response, after which we assume the user failed
-            and we abort the loop with a fail. THIS IS THE HARD BIT!
-        3. If we failed, tell the ui that we failed, and end the game.
-        4. If we succee take_turn again.
+        """
         (5. Future requirement level up)
         """
 
